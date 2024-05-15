c152e03142b17cc3a0004cdbe5af500b5c163315
harry <star.dev0333@gmail.com>
2024-05-16 00:38:43 +0900

    add: initial addcustomer api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Customers;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth')->group(function () {
+    // Customer
+    Route::post('add_customer', [Customers::class, 'add_customer'])->name('add_customer');
 });
