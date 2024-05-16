fd37b5ffc4242ab3477e0f8d22c6ccdd9102a677
harry <star.dev0333@gmail.com>
2024-05-16 20:49:18 +0900

    add: add addcustomer controller

--- app/Http/Controllers/Pages/Customers.php
+++ app/Http/Controllers/Pages/Customers.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Pages;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Customer;
 
 class Customers extends Controller
 {
@@ -20,6 +21,20 @@ class Customers extends Controller
 
   public function add_customer(Request $request)
   {
-    
+    $customer = new Customer();
+    $customer->user_id = $request->user()['id'];
+    $customer->first_name = $request->input('first_name');
+    $customer->last_name = $request->input('last_name');
+    $customer->email = $request->input('email');
+    $customer->phone = $request->input('phone');
+    $customer->status = 'Active'; // TODO
+    $customer->notes = $request->input('notes');
+    $customer->admin_notes = $request->input('admin_notes');
+
+    $customer->save();
+    return response()->json(
+      ['message' => 'Customer is saved successfully.'],
+      200
+    );
   }
 }
