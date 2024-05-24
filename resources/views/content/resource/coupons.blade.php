78f1979035f71e8b7aedad836816342bb135af55
cuteolef <star.dev0333@gmail.com>
2024-05-24 05:19:00 -0700

    fixed new coupon error

--- resources/views/content/resource/coupons.blade.php
+++ resources/views/content/resource/coupons.blade.php
@@ -132,83 +132,10 @@ $configData = Helper::appClasses();
             </div>
             @endforeach
         </div>
-        <div id="accordionIcon-1" class="accordion-collapse collapse" data-bs-parent="#accordionIcon">
-            <form action="{{route('resource-updatecoupons')}}" method="post" class="update-coupon">
-                @csrf
-                <div class="accordion-body">
-                    <div class="card-body">
-                        <div class="col-lg-12 d-flex mb-3">
-                            <div class="col-lg-3 px-1">
-                                <label for="defaultFormControlInput" class="form-label">Coupon Code</label>
-                                <input type="text" name="coupon_code" value="{{$coupon->coupon_code}}" class="form-control" id="defaultFormControlInput" placeholder="Coupon Code" aria-describedby="defaultFormControlHelp" />
-                            </div>
-                            <div class="col-lg-3 px-1">
-                                <label for="defaultFormControlInput" class="form-label">Name (For Internal Use)</label>
-                                <input type="text" name="coupon_name" value="{{$coupon->coupon_name}}" class="form-control" id="defaultFormControlInput" placeholder="Name (For Internal Use)" aria-describedby="defaultFormControlHelp" />
-                            </div>
-                            <div class="col-lg-3 px-1">
-                                <label for="defaultFormControlInput" class="form-label">Discount Value</label>
-                                <input type="text" name="discount_value" value="{{$coupon->discount_value}}" class="form-control" id="defaultFormControlInput" placeholder="Discount Value" aria-describedby="defaultFormControlHelp" />
-                            </div>
-                            <div class="col-lg-3 px-1">
-                                <label for="selectpickerBasic" class="form-label">Type</label>
-                                <select id="selectpickerBasic" name="discount_type" class="selectpicker w-100" data-style="btn-default">
-                                    <option value="percent">Percent</option>
-                                    <option value="fixed_value">Fixed Value</option>
-                                </select>
-                            </div>
-                        </div>
 
-                        <div class="coupon-use-restrictions">
-                            <h3>Coupon Use Restrictions:</h3>
-                            <div class="row d-flex">
-                                <div class="col-lg-3 px-1">
-                                    <label for="defaultFormControlInput" class="form-label">Use Limit Per Customer</label>
-                                    <input type="text" name="limit_per_customer" value="" class="form-control" id="defaultFormControlInput" placeholder="Use Limit Per Customer" aria-describedby="defaultFormControlHelp" />
-                                </div>
-                                <div class="col-lg-3 px-1">
-                                    <label for="defaultFormControlInput" class="form-label">Total Use Limit</label>
-                                    <input type="text" name="total_use_limit" value="" class="form-control" id="defaultFormControlInput" placeholder="Total Use Limit" aria-describedby="defaultFormControlHelp" />
-                                </div>
-                                <div class="col-lg-3 px-1">
-                                    <label for="defaultFormControlInput" class="form-label">Min Number of Customer Orders</label>
-                                    <input type="text" name="min_number" value="" class="form-control" id="defaultFormControlInput" placeholder="Min Number of Customer Orders" aria-describedby="defaultFormControlHelp" />
-                                </div>
-                                <div class="col-lg-3 px-1">
-                                    <label for="defaultFormControlInput" class="form-label">Max Number of Customer Orders</label>
-                                    <input type="text" name="max_number" value="" class="form-control" id="defaultFormControlInput" placeholder="Max Number of Customer Orders" aria-describedby="defaultFormControlHelp" />
-                                </div>
-                            </div>
-                            <div class="row d-flex">
-                                <div class="col-lg-3 px-1">
-                                    <label for="defaultFormControlInput" class="form-label">Agent IDs</label>
-                                    <input type="text" name="agentID" value="" class="form-control" id="defaultFormControlInput" placeholder="Agent IDs" aria-describedby="defaultFormControlHelp" />
-                                </div>
-                                <div class="col-lg-3 px-1">
-                                    <label for="defaultFormControlInput" class="form-label">Service IDs</label>
-                                    <input type="text" name="serviceID" value="" class="form-control" id="defaultFormControlInput" placeholder="Service IDs" aria-describedby="defaultFormControlHelp" />
-                                </div>
-                                <div class="col-lg-3 px-1">
-                                    <label for="defaultFormControlInput" class="form-label">Customer IDs</label>
-                                    <input type="text" name="customerID" value="" class="form-control" id="defaultFormControlInput" placeholder="Customer IDs" aria-describedby="defaultFormControlHelp" />
-                                </div>
-                                <div class="col-lg-3 px-1">
-                                    <label for="selectpickerBasic" class="form-label">Status</label>
-                                    <select id="selectpickerBasic" name="status" class="selectpicker w-100" data-style="btn-default">
-                                        <option value="active">Coupon is Active</option>
-                                        <option value="disabled">Coupon is Disabled</option>
-                                    </select>
-                                </div>
-                                <input type="text" name="id" value="{{$coupon->id}}" hidden/>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Save Coupon</button>
-                        <meta name="csrf-token" content="{{ csrf_token() }}">
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div id="add-coupon-box" class="add-coupon-box" data-os-action="coupons__new_form" data-os-output-target-do="append" data-os-output-target=".os-coupons-w">
+       
+
+        <div id="add-box" class="add-coupon-box" data-os-action="coupons__new_form" data-os-output-target-do="append" data-os-output-target=".os-coupons-w">
             <div class="add-coupon-graphic-w">
                 <div class="add-coupon-plus"><i class="latepoint-icon latepoint-icon-plus4 fa fa-plus"></i></div>
             </div>
@@ -220,9 +147,9 @@ $configData = Helper::appClasses();
 <script type="text/javascript" src="{{asset('/assets/jquery.js')}}"></script>
 <script type="text/javascript">
     $(document).ready(function() {
-        $('#add-coupon-box').click(function() {
+        $('#add-box').click(function() {
             var element = '<div id="accordionIcon" class="accordion mt-3 accordion-without-arrow"><div class="accordion-item card"><h2 class="accordion-header text-body d-flex justify-content-between" id="accordionIconOne"><button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#accordionIcon-2" aria-controls="accordionIcon-2">New Coupon</button></h2><div id="accordionIcon-2" class="accordion-collapse collapse" data-bs-parent="#accordionIcon"><div class="accordion-body"><div class="card-body"><div class="col-lg-12 d-flex mb-3"><div class="col-lg-3 px-1"><label for="defaultFormControlInput" class="form-label">Coupon Code</label><input type="text" class="form-control" id="defaultFormControlInput" placeholder="Coupon Code" aria-describedby="defaultFormControlHelp" /></div><div class="col-lg-3 px-1"><label for="defaultFormControlInput" class="form-label">Name (For Internal Use)</label><input type="text" class="form-control" id="defaultFormControlInput" placeholder="Name (For Internal Use)" aria-describedby="defaultFormControlHelp" /></div><div class="col-lg-3 px-1"><label for="defaultFormControlInput" class="form-label">Discount Value</label><input type="text" class="form-control" id="defaultFormControlInput" placeholder="Discount Value" aria-describedby="defaultFormControlHelp" /></div><div class="col-lg-3 px-1"><label for="selectpickerBasic" class="form-label">Type</label><select id="selectpickerBasic" class="selectpicker w-100" data-style="btn-default"><option>Percent</option><option>Fixed Value</option></select></div></div><div class="coupon-use-restrictions"><h3>Coupon Use Restrictions:</h3><div class="row d-flex"><div class="col-lg-3 px-1"><label for="defaultFormControlInput" class="form-label">Use Limit Per Customer</label><input type="text" class="form-control" id="defaultFormControlInput" placeholder="Use Limit Per Customer" aria-describedby="defaultFormControlHelp" /></div><div class="col-lg-3 px-1"><label for="defaultFormControlInput" class="form-label">Total Use Limit</label><input type="text" class="form-control" id="defaultFormControlInput" placeholder="Total Use Limit" aria-describedby="defaultFormControlHelp" /></div><div class="col-lg-3 px-1"><label for="defaultFormControlInput" class="form-label">Min Number of Customer Orders</label><input type="text" class="form-control" id="defaultFormControlInput" placeholder="Min Number of Customer Orders" aria-describedby="defaultFormControlHelp" /></div><div class="col-lg-3 px-1"><label for="defaultFormControlInput" class="form-label">Max Number of Customer Orders</label><input type="text" class="form-control" id="defaultFormControlInput" placeholder="Max Number of Customer Orders" aria-describedby="defaultFormControlHelp" /></div></div><div class="row d-flex"><div class="col-lg-3 px-1"><label for="defaultFormControlInput" class="form-label">Agent IDs</label><input type="text" class="form-control" id="defaultFormControlInput" placeholder="Agent IDs" aria-describedby="defaultFormControlHelp" /></div><div class="col-lg-3 px-1"><label for="defaultFormControlInput" class="form-label">Service IDs</label><input type="text" class="form-control" id="defaultFormControlInput" placeholder="Service IDs" aria-describedby="defaultFormControlHelp" /></div><div class="col-lg-3 px-1"><label for="defaultFormControlInput" class="form-label">Customer IDs</label><input type="text" class="form-control" id="defaultFormControlInput" placeholder="Customer IDs" aria-describedby="defaultFormControlHelp" /></div><div class="col-lg-3 px-1"><label for="selectpickerBasic" class="form-label">Status</label><select id="selectpickerBasic" class="selectpicker w-100" data-style="btn-default"><option>Coupon is Active</option><option>Coupon is Disabled</option></select></div></div></div><button type="button" class="btn btn-primary">Save Coupon</button></div></div></div>';
-            $("#add-coupon-box").before(element);
+            $("#add-box").before(element);
         });
     });
 
