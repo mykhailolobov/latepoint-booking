c5789e64ff488ed62a5cdb8921c9277105f5cbc8
cuteolef <star.dev0333@gmail.com>
2024-05-28 19:51:38 -0700

    check profile password

--- app/Http/Controllers/Settings/Profile.php
+++ app/Http/Controllers/Settings/Profile.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
-
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
 
 class Profile extends Controller
 {
@@ -55,6 +56,7 @@ class Profile extends Controller
         $user = Auth::user();
         $user_id = $user->id;
         $currentUser = User::findOrFail($user_id);
+        
 
         return view('content.settings.editprofile', compact('currentUser'));
     }
@@ -62,9 +64,35 @@ class Profile extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
-        //
+        $user = User::findOrFail($request->id);
+
+        $validatedData = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'string|nullable|max:255',
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique('users')->ignore($user->id), // Unique email validation, ignoring the current user
+            ],
+            'new_password' => 'nullable|string|max:255|min:8',
+            'confirm_password' => 'same:new_password|max:255'
+
+        ]);
+
+        $user->first_name = $validatedData['first_name'];
+        $user->last_name = $validatedData['last_name'];
+        $user->email = $validatedData['email'];
+
+        if ($request->filled('new_password')) {
+            $user->password = Hash::make($validatedData['new_password']);
+            $user->save();
+        } else {
+            $user->save();
+        }        
     }
 
     /**
